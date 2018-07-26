df6d9563a1208b2ac4813e1305d76653d086181e
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-07-26 09:53:12 +0200

    Fixed Licence headers.
    Signed-off-by: Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>

--- src/main/java/de/fraunhofer/iosb/ilt/stp/processors/aggregation/AggregationBase.java
+++ src/main/java/de/fraunhofer/iosb/ilt/stp/processors/aggregation/AggregationBase.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2018 Fraunhofer IOSB
+ * Copyright (C) 2018 Fraunhofer Institut IOSB, Fraunhoferstr. 1, D 76131
+ * Karlsruhe, Germany.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
